e3348cf106a535f0de22727a50dcc6229ca6e92d
Gustavo Pabón <gustavo.pabon@gmail.com>
2016-09-07 19:21:11 +0000

    clean repository

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,6 +6,7 @@ echo --- GCM PoC ---------------------------------------------
 workingDir=`dirname $0`
 . ${workingDir}/env.sh
 
+JAVACMD=$JAVACMD" -Dgcm.provider=org.objectweb.proactive.core.component.Fractive"
 $JAVACMD test.gpabon.gcmpoc.Main
 
 echo
